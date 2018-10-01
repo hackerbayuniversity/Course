feb5ebf4e84527bd6999b0eefee7194e48272c93
Ivan S <ivanms1@gmail.com>
2018-10-01 16:30:44 +0700

    removed submodule

---
+++ tasks/task-8/code/backend/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const passport = require('passport');
+
+const users = require('./routes/users');
+const websites = require('./routes/websites');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+if(process.env.NODE_ENV !== 'test') app.use(logger('tiny'));
+
+app.use(passport.initialize());
+
+require('./passport/')(passport);
+
+app.use('/users', users);
+app.use('/websites', websites);
+
+app.get('/', (req, res) => {
+	res.json({ success: true })
+});
+
+app.listen(3001, () => console.log('Listening on Port 3001'));
+
+module.exports = app;
